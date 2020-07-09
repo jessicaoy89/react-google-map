88e01d3e73150f855f3da803baf01287e05d8347
Larry <yuetao1998@gmail.com>
2020-07-08 20:21:57 -0500

    using google maps javascript Api to create a map page that can display spots and when clicking on each spot, an info window is shown

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import mapStyles from "./styles/mapStyles";
+import {
+    GoogleMap,
+    useLoadScript,
+    Marker,
+    InfoWindow,
+} from "@react-google-maps/api"
+// import {formatRelative} from "date-fns";
+// import "@reach/combobox/styles.css";
+const libraries = ["places"];
+const mapContainerStyle = {
+    width: "100vw",
+    height: "100vh",
 }
+const center = {
+    lat: 43.653225,
+    lng: -79.383186,
+}
+const options = {
+    styles: mapStyles,
+    disableDefaultUI: true,
+    zoomControl: true,
+}
+export default function App() {
 
-export default App;
+    const {isLoaded, loadError} = useLoadScript({
+        googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+        libraries,
+    });
+    const [markers, setMarkers] = React.useState([]);
+    const [selected, setSelected] = React.useState(null);
+    // const mapRef = React.useRef();
+    // const onMapLoad = React.useCallback((map) => {
+    //     mapRef.current = map;
+    // }, []);
+    if (loadError) return "Error loading maps";
+    if (!isLoaded) return "Loading Maps";
+    return (
+        <div>
+            <h1>Travel Planner</h1>
+            <GoogleMap
+                mapContainerStyle={mapContainerStyle}
+                zoom={12}
+                center={center}
+                options={options}
+                onClick={(event) => {
+                    setMarkers(prev => [...prev, {
+                        lat: event.latLng.lat(),
+                        lng: event.latLng.lng(),
+                        time: new Date(),
+                    }])
+                }}>
+                {markers.map(marker => <Marker
+                    key={marker.time.toISOString()}
+                    position={{lat: marker.lat, lng: marker.lng}}
+                    onClick={() => {
+                        setSelected(marker);
+                    }}
+                />)}
+                {selected ? (<InfoWindow
+                    position={{lat: selected.lat, lng: selected.lng}}
+                    // set the selected back to null when closing the info window
+                    onCloseClick={() => {setSelected(null);}}>
+                    <div>
+                        <h2>Spot</h2>
+                        <p>Some info about that spot...</p>
+                    </div>
+                </InfoWindow>) : null}
+            </GoogleMap>
+        </div>
+    )
+};
